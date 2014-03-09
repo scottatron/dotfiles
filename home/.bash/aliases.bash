a7387c260ac81e4d604b5a63452c7727eb924541
Scott Arthur <scott@scottatron.com>
2014-03-09 23:57:22 +0000

    Aliases

---
+++ home/.bash/aliases.bash
@@ -0,0 +1,13 @@
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias be="bundle exec"
+alias gs="git status"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias gc="git commit"
+alias gf="git fixws"
+alias ga="git add"
+alias gall="git add --all"
+alias gco="git checkout"
