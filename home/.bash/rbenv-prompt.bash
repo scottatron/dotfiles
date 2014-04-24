8a51dbb3b876a2bb6b8599d6bad39066f3bb143d
Scott Arthur <scott@scottatron.com>
2014-04-24 12:40:19 +1000

    Added git info to prompt

--- home/.bash/rbenv-prompt.bash
+++ home/.bash/rbenv-prompt.bash
@@ -6,10 +6,18 @@ __rbenv_ps1 ()
   printf "|$rbenv_ruby_version|"
 }
 
+# Load git-prompt.sh from Homebrew if it exists
+if [[ ! -z $(which brew) ]]; then
+  GIT_PROMPT_SH="$(brew --prefix)/etc/bash_completion.d/git-prompt.sh"
+  if [[ -f $GIT_PROMPT_SH ]]; then
+    . $GIT_PROMPT_SH
+  fi
+fi
+
 # prompt with git && rbenv
-if [ -f $BASH_COMPLETION_DIR/git ] && [ -f `which rbenv` ]; then
+if [ ! -z $GIT_PROMPT_SH ] && [ -f `which rbenv` ]; then
   export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w$(__git_ps1) $(__rbenv_ps1) \n\[\033[01;34m\]\$\[\033[00m\] '
-elif [ -f $BASH_COMPLETION_DIR/git ]; then
+elif [ ! -z $GIT_PROMPT_SH ]; then
   export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w$(__git_ps1) \n\[\033[01;34m\]\$\[\033[00m\] '
 elif [ `which rbenv` ]; then
   export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w $(__rbenv_ps1) \n\[\033[01;34m\]\$\[\033[00m\] '
