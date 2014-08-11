56dddb4472f58669cff1557f118c9a61de459029
Scott Arthur <scott@scottatron.com>
2014-08-11 16:51:44 +1000

    Updated prompt so git prompt still works on Ubuntu even if rbenv isn't installed

--- home/.bash/rbenv-prompt.bash
+++ home/.bash/rbenv-prompt.bash
@@ -14,12 +14,24 @@ if [[ ! -z $(which brew) ]]; then
   fi
 fi
 
+# Ubuntu
+if $(uname -a | grep -q 'ubuntu'); then
+  GIT_PROMPT_SH="/usr/lib/git-core/git-sh-prompt"
+  if [[ -f "/usr/lib/git-core/git-sh-prompt" ]]; then
+    . "/usr/lib/git-core/git-sh-prompt"
+  else
+    GIT_PROMPT_SH=""
+  fi
+fi
+
+rbenv_is_installed=$(which rbenv 2>&1)
+
 # prompt with git && rbenv
-if [ ! -z $GIT_PROMPT_SH ] && [ -f `which rbenv` ]; then
+if [ ! -z $GIT_PROMPT_SH ] && [[ ! -z $rbenv_is_installed ]]; then
   export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w$(__git_ps1) $(__rbenv_ps1) \n\[\033[01;34m\]\$\[\033[00m\] '
 elif [ ! -z $GIT_PROMPT_SH ]; then
   export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w$(__git_ps1) \n\[\033[01;34m\]\$\[\033[00m\] '
-elif [ `which rbenv` ]; then
+elif [[ ! -z $rbenv_is_installed ]]; then
   export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w $(__rbenv_ps1) \n\[\033[01;34m\]\$\[\033[00m\] '
 else
   export PS1='\[\033[01;32m\]\u@\h\[\033[01;33m\] \w \n\[\033[01;34m\]\$\[\033[00m\] '
