d26dca38db510cf705724c51165af8c509a26888
Scott Arthur <scott@scottatron.com>
2014-08-17 00:35:34 +1000

    Add 'gb' alias for git branch

--- home/.bash/aliases.bash
+++ home/.bash/aliases.bash
@@ -19,20 +19,22 @@ fi
 if $(type __git_complete 2>&1 | grep -q 'not found'); then
   echo "No Git completion found, skipping Git aliases"
 else
-  alias gs="git status"
-  alias gd="git diff"
-  __git_complete gd _git_diff
-  alias gdc="git diff --cached"
-  alias gc="git commit"
-  __git_complete gc _git_commit
-  alias gf="git fixws"
   alias ga="git add"
-  __git_complete ga _git_add
   alias gall="git add --all"
+  alias gb="git branch"
+  alias gc="git commit"
   alias gco="git checkout"
-  __git_complete gco _git_checkout
+  alias gd="git diff"
+  alias gdc="git diff --cached"
+  alias gf="git fixws"
   alias gp="git push"
+  alias gs="git status"
+  __git_complete ga _git_add
+  __git_complete gc _git_commit
+  __git_complete gco _git_checkout
+  __git_complete gd _git_diff
   __git_complete gp _git_push
+  __git_complete gb _git_branch
 fi
 
 
